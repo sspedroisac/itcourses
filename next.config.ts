b9c5829594f7e698a9affbde7de7bd8a0c21d0c8
Pedro Isac <ss.pedroisac@gmail.com>
2026-07-27 20:50:11 -0300

    undo: fix  building

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  outputFileTracingIncludes: {
-    "/api/**/*": ["./content/**/*"],
-  },
+  /* config options here */
 };
 
 export default nextConfig;
