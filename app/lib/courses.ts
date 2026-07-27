0d2b1bb11f1ad055eb493a5db8b93dea5d8f4311
Pedro Isac <ss.pedroisac@gmail.com>
2026-07-27 20:45:50 -0300

    fix: building

--- app/lib/courses.ts
+++ app/lib/courses.ts
@@ -1,7 +1,7 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 
-import fs from "fs/promises";
-import path from "path";
+import fs from "node:fs/promises";
+import path from "node:path";
 
 const getTotalClassesCount = (modules: any[]) => {
   return modules.reduce((acc, module) => acc + module.classes.length, 0);
--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  outputFileTracingIncludes: {
+    "/api/**/*": ["./content/**/*"],
+  },
 };
 
 export default nextConfig;
 
-import('@opennextjs/cloudflare').then(m => m.initOpenNextCloudflareForDev());
+import("@opennextjs/cloudflare").then((m) => m.initOpenNextCloudflareForDev());
