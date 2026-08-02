897f9d4ea9ad4538fdaabe158ea087d783e65266
Pedro Isac <ss.pedroisac@gmail.com>
2026-08-02 14:13:05 -0300

    add: next-intl

--- app/(site)/[locale]/layout.tsx
+++ app/(site)/[locale]/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import Header from "../../components/Header";
 import Footer from "../../components/Footer";
+import { NextIntlClientProvider } from "next-intl";
+import { getMessages } from "next-intl/server";
 import "../../globals.css";
 
 export const metadata: Metadata = {
@@ -8,17 +10,21 @@ export const metadata: Metadata = {
   description: "A platform for IT courses and tutorials.",
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const messages = await getMessages();
+
   return (
-    <html lang="en" className="h-full antialiased">
+    <html className="h-full antialiased">
       <body className="min-h-full flex flex-col">
-        <Header />
-        {children}
-        <Footer />
+        <NextIntlClientProvider messages={messages}>
+          <Header />
+          {children}
+          <Footer />
+        </NextIntlClientProvider>
       </body>
     </html>
   );
--- app/(site)/page.tsx
+++
@@ -1,5 +0,0 @@
-import { redirect } from "next/navigation";
-
-export default function InitPage() {
-  return redirect(`/en`);
-}
