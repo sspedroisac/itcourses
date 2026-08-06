1c4ac8124237c5b50e5ba8881d16833d3c8e40d3
Pedro Isac <ss.pedroisac@gmail.com>
2026-08-06 19:57:49 -0300

    add: experimental-edge

--- middleware.ts
+++ middleware.ts
@@ -3,7 +3,7 @@ import { routing } from "./i18n/routing";
 
 export default createMiddleware(routing);
 
-export const runtime = "edge";
+export const runtime = "experimental-edge";
 
 export const config = {
   matcher: ["/((?!api|_next|.*\\..*).*)"],
